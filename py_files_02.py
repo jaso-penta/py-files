095f88c8709b30b16e0431aca02c0079f2e95571
Jašo <andrija.pericic@gmail.com>
2025-10-03 18:57:09 +0200

    Update py_files_02.py

--- py_files_02.py
+++ py_files_02.py
@@ -8,11 +8,16 @@ while True:
 
     contact = f'Ime: {first_name}; Prezime: {last_name}; Phone: {phone}; Email: {email}\n'
 
-    print(contact)
-
-    file_writer = open('contacts.txt', 'a')
-    file_writer.write(contact)
-    file_writer.close()
+    try:
+        file_writer = open('contacts.txt', 'a')
+        file_writer.write(contact)
+        
+        x = 5 / 0
+    except Exception as ex:
+        print(f'Dogodila se greska {ex}! ')
+    finally:
+        print('Pozdrav iz finally')
+        file_writer.close()
 
     next_contact = input('Zelite li unijeti novi kontakt ? (Da/Ne)')
     if next_contact.lower() != 'da':
