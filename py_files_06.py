d571b09a9b27c38a9f4c6a8cfb561ad628e93c2e
Jašo <andrija.pericic@gmail.com>
2025-10-03 19:55:01 +0200

    Create py_files_06.py

---
+++ py_files_06.py
@@ -0,0 +1,15 @@
+# Snippet za citanje podataka
+
+try:
+    with open('contacts.txt', 'r') as file_reader:
+        file_content = file_reader.readlines()
+
+        for line in file_content:
+            print(f'Sadrzaj linije {line}')
+            line_elements = line.split(';')
+            for line_element in line_elements:
+                print(line_element)
+
+
+except Exception as ex:
+    print(f'Dogodila se greska {ex}! ')
